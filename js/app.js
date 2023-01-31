6b41b907e552ee3d1e328199b8f31c08ba605f9a
Subramanya K S <subramanyaks2205@gmail.com>
2023-01-31 18:06:11 +0530

    updated round

--- js/app.js
+++ js/app.js
@@ -1,118 +1,155 @@
-const computerChoiceDisplay = document.getElementById('computer-choice');
-const userChoiceDisplay = document.getElementById('user-choice');
-const resultDisplay = document.getElementById('result');
-const possibleChoices = document.querySelectorAll('button');
-const tr= document.getElementById('total');
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+const tr = document.getElementById("total");
+const round = document.getElementById("round");
+const rk = document.getElementById("rock1");
+const pa = document.getElementById("paper1");
+const sc = document.getElementById("scissor1");
+const cs = document.getElementById("computer-score");
+const us = document.getElementById("user-score");
 
-const rk= document.getElementById('rock1');
-const pa = document.getElementById('paper1');
-const sc = document.getElementById('scissor1');
-
-const cs = document.getElementById('computer-score');
-const us = document.getElementById('user-score');
-let cc=0;
-let uc=0;
-
-
-rk.addEventListener('click', function () {
-    userChoice="rock";
-    userChoiceDisplay.innerHTML=userChoice;
-    generateComputerChoice();
-    getResult();
-})
-pa.addEventListener('click', function () {
-    userChoice="paper";
-    userChoiceDisplay.innerHTML=userChoice;
-    generateComputerChoice();
-    getResult();
-})
-sc.addEventListener('click', function () {
-    userChoice="scissor";
-    userChoiceDisplay.innerHTML=userChoice;
-    generateComputerChoice();
-    getResult();
-})
+let cc = 0;
+let uc = 0;
+let roun = 0;
+let curr = 0;
 
 let userChoice;
 let computerChoice;
 let result;
 let tt;
 
+round.addEventListener("change", function (e) {
+  e.preventDefault();
+  console.log(e.target.value);
+  roun = e.target.value;
+});
 
-function generateComputerChoice(){
-    const randomNumber = Math.floor(Math.random() * 3)+1;
-    //console.log(randomNumber);
-    
-    if(randomNumber===1){
-        computerChoice="rock";
+rk.addEventListener("click", function () {
+  userChoice = "rock";
+  userChoiceDisplay.innerHTML = userChoice;
+  if (roun > 0) {
+    if (curr < roun) {
+      generateComputerChoice();
+      getResult();
+      curr++;
+      console.log(curr);
+    } else {
+      finishGame();
     }
-    if(randomNumber===2){
-        computerChoice="paper";
+  }
+});
+pa.addEventListener("click", function () {
+  userChoice = "paper";
+  userChoiceDisplay.innerHTML = userChoice;
+  if (roun > 0) {
+    if (curr < roun) {
+      generateComputerChoice();
+      getResult();
+      curr++;
+      console.log(curr);
+    } else {
+      finishGame();
     }
-    if(randomNumber===3){
-        computerChoice="scissor";
+  }
+});
+sc.addEventListener("click", function () {
+  userChoice = "scissor";
+  userChoiceDisplay.innerHTML = userChoice;
+  if (roun > 0) {
+    if (curr < roun) {
+      generateComputerChoice();
+      getResult();
+      curr++;
+      console.log(curr);
+    } else {
+      finishGame();
     }
-    computerChoiceDisplay.innerHTML=computerChoice;
-    
+  }
+});
 
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  if (randomNumber === 1) {
+    computerChoice = "rock";
+  }
+  if (randomNumber === 2) {
+    computerChoice = "paper";
+  }
+  if (randomNumber === 3) {
+    computerChoice = "scissor";
+  }
+  computerChoiceDisplay.innerHTML = computerChoice;
 }
 
-function reset(){
-    window.location.reload();
+function reset() {
+  window.location.reload();
 }
 
-function getResult(){
-    if(userChoice===computerChoice){
-        result="It's a Draw 🤝";
-        cc==cc;
+function getResult() {
+  if (userChoice === computerChoice) {
+    result = "It's a Draw 🤝";
+    cc == cc;
+  }
+  if (computerChoice === "rock" && userChoice === "paper") {
+    result = "You Win ✔";
+    uc += 1;
+  }
+  if (computerChoice === "rock" && userChoice === "scissor") {
+    result = "You Lose ✖";
+    cc += 1;
+    uc == uc;
+  }
+  if (computerChoice === "paper" && userChoice === "rock") {
+    result = "You Lose ✖";
+    cc += 1;
+  }
+  if (computerChoice === "paper" && userChoice === "scissor") {
+    result = "You Win ✔";
+    uc += 1;
+  }
+  if (computerChoice === "scissor" && userChoice === "paper") {
+    result = "You Lose ✖";
+    cc += 1;
+  }
+  if (computerChoice === "scissor" && userChoice === "rock") {
+    result = "You Win ✔";
+    uc += 1;
+  }
 
-    }
-    if(computerChoice==="rock" && userChoice==="paper"){
-        result="You Win ✔";
-        uc+=1;
-    }
-    if(computerChoice==="rock" && userChoice==="scissor"){
-        result="You Lose ✖";
-        cc+=1;
-        uc==uc;
-    }
-    if(computerChoice==="paper" && userChoice==="rock"){
-        result="You Lose ✖";
-        cc+=1;
-    }
-    if(computerChoice==="paper" && userChoice==="scissor"){
-        result="You Win ✔";
-        uc+=1;
-    }
-    if(computerChoice==="scissor" && userChoice==="paper"){
-        result="You Lose ✖";
-        cc+=1;
-    }
-    if(computerChoice==="scissor" && userChoice==="rock"){
-        result="You Win ✔";
-        uc+=1;
-    }
-    
-    resultDisplay.innerHTML=result;
-    cs.innerHTML=cc;
-    us.innerHTML=uc;
-    // if(cc>=5){
-    //     tt="Oh Sorry,Computer defeated You,Please Try again";
-    //     tr.innerHTML=tt;
-        
-    // }
-    // if(uc>=5){
-    //     tt="Congratulation,You defeated Computer,";
-    //     tr.innerHTML=tt;
-    // }
+  resultDisplay.innerHTML = result;
+  cs.innerHTML = cc;
+  us.innerHTML = uc;
 }
 
-function info(){
-    // if(window.getComputedStyle(inform).display==='none'){
-       var infor=  document.getElementById('info');
-       infor.classList.toggle("mystyle");
-    // }
-    // if(window.getComputedStyle(inform).display === 'block'){
-    //     document.getElementById('info').style.display='none';
-    // }
+function finishGame() {
+    console.log("user: ",us);
+    console.log("compu : ",cs);
+  if (uc < cc) {
+
+    resultDisplay.innerHTML = " ";
+    tr.innerHTML = `OH NO, YOU LOST THE GAME!`;
+    setTimeout(() => {
+      reset();
+    }, 5000);
+  } else if (uc > cc) {
+    resultDisplay.innerHTML = " ";
+    tr.innerHTML = `CONGRATULATIONS, YOU WON THE GAME! `;
+    setTimeout(() => {
+      reset();
+    }, 5000);
+  } else {
+    resultDisplay.innerHTML = "";
+    tr.innerHTML = `THIS GAME WAS A DRAW!`;
+    setTimeout(() => {
+      reset();
+    }, 5000);
+  }
+}
+
+function info() {
+  var infor = document.getElementById("info");
+  infor.classList.toggle("mystyle");
 }
